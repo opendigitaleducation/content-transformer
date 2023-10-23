0b179d40a3845e983327b5917c4dee65175bcea0
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2023-10-23 10:51:57 +0200

    feat: #SRE-1470, attempt at multithreading

--- index.ts
+++ index.ts
@@ -3,37 +3,50 @@ import express, { Express, Request, Response } from 'express';
 import 'global-jsdom/register';
 import { transformController } from './src/controllers/transformation-controller.js';
 import { initMetrics } from './src/controllers/metrics-controller.js';
+import cluster  from 'cluster'
+import os from 'os';
+const numCPUs = os.cpus().length;
 
-dotenv.config();
+if (cluster.isPrimary) {
+  // Fork workers for each CPU core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker: any, code: number, signal: any) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  dotenv.config();
 
-const app: Express = express()
-const port = process.env.PORT || 3000;
-const serviceVersion = 1;
+  const app: Express = express()
+  const port = process.env.PORT || 3000;
+  const serviceVersion = 1;
 
-initMetrics(app);
+  initMetrics(app);
 
-const expressOptions: any = {}
-if (process.env.MAX_BODY_SIZE) {
-  expressOptions['limit'] = process.env.MAX_BODY_SIZE
+  const expressOptions: any = {}
+  if (process.env.MAX_BODY_SIZE) {
+    expressOptions['limit'] = process.env.MAX_BODY_SIZE
+  }
+
+
+  app.use(express.json(expressOptions));
+  app.use((err: Error, req: Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'internal.server.error' });
+  });
+
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+  });
+
+
+  app.post('/transform', (req: Request, res: Response) => {
+    transformController(req, res, serviceVersion);
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 }
-
-
-app.use(express.json(expressOptions));
-app.use((err: Error, req: Request, res: express.Response, next: express.NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'internal.server.error' });
-});
-
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-});
-
-
-app.post('/transform', (req: Request, res: Response) => {
-  transformController(req, res, serviceVersion);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
