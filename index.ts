69fd510bef3a7a85049c4b7288e922ffa922b35d
Clement Creusat <clement.creusat@gmail.com>
2024-01-23 11:48:57 +0100

    chore: update deps
    add license, fix tiptap version 2.0.3, update branch tag develop

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import express, {  } from 'express';
-import 'global-jsdom/register';
-import cluster  from 'cluster'
-import os from 'os';
-import createServer from './src/server.js';
-import { AggregatorRegistry } from 'prom-client';
+import dotenv from "dotenv";
+import express from "express";
+import "global-jsdom/register";
+import cluster from "cluster";
+import os from "os";
+import createServer from "./src/server.js";
+import { AggregatorRegistry } from "prom-client";
 
 dotenv.config();
 
@@ -12,7 +12,9 @@ const metricsServer = express();
 const aggregatorRegistry = new AggregatorRegistry();
 
 const numCPUs = os.cpus().length;
-const desiredInstances = process.env.NB_THREADS ? parseInt(process.env.NB_THREADS) : numCPUs;
+const desiredInstances = process.env.NB_THREADS
+  ? parseInt(process.env.NB_THREADS)
+  : numCPUs;
 const metricsPort = process.env.METRICS_PORT || 3001;
 
 if (cluster.isPrimary && desiredInstances > 1) {
@@ -21,26 +23,25 @@ if (cluster.isPrimary && desiredInstances > 1) {
   for (let i = 0; i < desiredInstances; i++) {
     cluster.fork();
   }
-  cluster.on('exit', (worker: any, code: number, signal: any) => {
+  cluster.on("exit", (worker: any, code: number, signal: any) => {
     console.log(`Worker ${worker.process.pid} died`);
   });
 
-  metricsServer.get('/metrics', async (req, res) => {
-		try {
-			const metrics = await aggregatorRegistry.clusterMetrics();
-			res.set('Content-Type', aggregatorRegistry.contentType);
-			res.send(metrics);
-		} catch (e) {
-      console.error(e)
-			res.statusCode = 500;
-			res.send("metrics.not.available");
-		}
-	});
-	metricsServer.listen(metricsPort);
-	console.log(
-		`Cluster metrics server listening to ${metricsPort}, metrics exposed on /metrics`,
-	);
-
+  metricsServer.get("/metrics", async (req, res) => {
+    try {
+      const metrics = await aggregatorRegistry.clusterMetrics();
+      res.set("Content-Type", aggregatorRegistry.contentType);
+      res.send(metrics);
+    } catch (e) {
+      console.error(e);
+      res.statusCode = 500;
+      res.send("metrics.not.available");
+    }
+  });
+  metricsServer.listen(metricsPort);
+  console.log(
+    `Cluster metrics server listening to ${metricsPort}, metrics exposed on /metrics`
+  );
 } else {
-  createServer()
+  createServer();
 }
